9943ae4c71af066d190c5e361d9d52f1397ba87e
Umisyo <s_kusunoki@yumemi.co.jp>
2023-04-28 22:22:24 +0900

    feat: メイン機能を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+/// Simple command like a echo
+#[derive(Parser)]
+struct Args {
+    /// value to display
+    value: Vec<String>,
+
+    ///no new line
+    #[arg(short, long)]
+    no_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let output: String = args.value.join(" ");
+
+    if args.no_newline {
+        print!("{}", output);
+    }
+    else {
+        println!("{}", output);
+    }
 }
